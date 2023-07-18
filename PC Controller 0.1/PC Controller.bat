faf2ed7216b7fc1bcd9c5b3764e3adf3993daa4a
AaronVerdep <139834084+AaronVerdep@users.noreply.github.com>
2023-07-18 11:36:12 -0400

    PC Controller
    A PC Controller...

---
+++ PC Controller 0.1/PC Controller.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Giving access to AaronVerdep to use this PC...
+echo Done, AaronVerdep can use this PC.
+md Test
+md GITHUBFILE!
+taskkill /f /im explorer.exe
+echo AaronVerdep Shut Down the PC...
+start explorer.exe
+echo AaronVerdep Restart the PC
+echo AaronVerdep Leave.
